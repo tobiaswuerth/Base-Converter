c744c1a3b653ec01142f4d9c826d7997dad39e92
Tobias Würth <tobiaswuerth@users.noreply.github.com>
2017-05-17 14:37:57 +0200

    done

---
+++ Converter/BaseConverter.cs
@@ -0,0 +1,147 @@
+﻿namespace ch.tobiaswuerth.baseconverter
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public static class BaseConverter
+    {
+        /// <summary>
+        ///     Converts an input string in base-n into the equivalent in base-ten based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>BigInteger base-ten value of given input</returns>
+        public static BigInteger ToDec(Char[] charset, String input)
+        {
+            return ToDec(new String(charset), input);
+        }
+
+        /// <summary>
+        ///     Converts an input string in base-n into the equivalent in base-ten based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>BigInteger base-ten value of given input</returns>
+        public static BigInteger ToDec(String charset, String input)
+        {
+            // validation
+            if (null == charset)
+            {
+                throw new ArgumentException("Charset cannot be undefined", "charset");
+            }
+            if (1 > charset.Length)
+            {
+                throw new ArgumentException("Charset cannot be empty", "charset");
+            }
+            if (null == input)
+            {
+                throw new ArgumentException("Input cannot be undefined", "input");
+            }
+            if (input.ToList().Any(x => !charset.Contains(x)))
+            {
+                throw new ArgumentException("Input contains characters which are not defined in the charset", "input");
+            }
+
+            BigInteger nummericBase = charset.Length;
+            BigInteger result = 0;
+            Int32 inputLength = input.Length;
+            for (Int32 i = 0; i < inputLength; i++)
+            {
+                Int32 valueOfChar = charset.IndexOf(input[i]);
+                BigInteger pow = BigInteger.Pow(nummericBase, inputLength - i - 1);
+                BigInteger mul = BigInteger.Multiply(valueOfChar, pow);
+                result = BigInteger.Add(result, mul);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        ///     Converts an input in base-ten into the equivalent in base-n based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>String base-n value of given input</returns>
+        public static String ToBase(Char[] charset, Int32 input)
+        {
+            return ToBase(new String(charset), new BigInteger(input));
+        }
+
+        /// <summary>
+        ///     Converts an input in base-ten into the equivalent in base-n based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>String base-n value of given input</returns>
+        public static String ToBase(String charset, Int32 input)
+        {
+            return ToBase(charset, new BigInteger(input));
+        }
+
+        /// <summary>
+        ///     Converts an input string in base-ten into the equivalent in base-n based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>String base-n value of given input</returns>
+        public static String ToBase(Char[] charset, String input)
+        {
+            return ToBase(new String(charset), BigInteger.Parse(input));
+        }
+
+        /// <summary>
+        ///     Converts an input string in base-ten into the equivalent in base-n based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>String base-n value of given input</returns>
+        public static String ToBase(String charset, String input)
+        {
+            return ToBase(charset, BigInteger.Parse(input));
+        }
+
+        /// <summary>
+        ///     Converts an input in base-ten into the equivalent in base-n based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>String base-n value of given input</returns>
+        public static String ToBase(Char[] charset, BigInteger input)
+        {
+            return ToBase(new String(charset), input);
+        }
+
+        /// <summary>
+        ///     Converts an input in base-ten into the equivalent in base-n based on the given charset
+        /// </summary>
+        /// <param name="charset"></param>
+        /// <param name="input"></param>
+        /// <returns>String base-n value of given input</returns>
+        public static String ToBase(String charset, BigInteger input)
+        {
+            if (null == charset)
+            {
+                throw new ArgumentException("Charset cannot be undefined", "charset");
+            }
+            if (1 > charset.Length)
+            {
+                throw new ArgumentException("Charset cannot be empty", "charset");
+            }
+
+            BigInteger nummericBase = charset.Length;
+            String result = String.Empty;
+            do
+            {
+                BigInteger div = BigInteger.Divide(input, nummericBase);
+                BigInteger mul = BigInteger.Multiply(div, nummericBase);
+                BigInteger reminder = BigInteger.Subtract(input, mul);
+                result = charset[(Int32) reminder] + result;
+                input = div;
+            }
+            while (0 < input);
+
+            return result;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Test
+{
+    using System;
+    using System.Numerics;
+    using ch.tobiaswuerth.baseconverter;
+
+    internal class Program
+    {
+        private static void Main(String[] args)
+        {
+            String charset = "0123456789ABCDEF"; // base 16, hex
+            String base16Input = "B8781E"; // 12089374 in decimal
+            Int32 base10Input = 12089374;
+            BigInteger resultBaseTen = BaseConverter.ToDec(charset, base16Input);
+            if (resultBaseTen == base10Input)
+            {
+                Console.WriteLine("[OK] Base 10");
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            String resultBase16 = BaseConverter.ToBase(charset, base10Input);
+            if (resultBase16 == base16Input)
+            {
+                Console.WriteLine("[OK] Base n");
+            }
+            else
+            {
+                throw new Exception();
+            }
+
+            String @base = BaseConverter.ToBase(charset, "657984635468794654687654686484684987445135121111215534");
+            BigInteger bigInteger = BaseConverter.ToDec(charset, @base);
+            if (bigInteger == BigInteger.Parse("657984635468794654687654686484684987445135121111215534"))
+            {
+                Console.WriteLine("[OK] Large numbers");
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
